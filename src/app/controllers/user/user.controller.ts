22e0fc78190d827b428778567d6e3ab85f093570
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-10-04 20:43:11 -0300

    chore: move models dir

--- src/app/controllers/user/user.controller.ts
+++ src/app/controllers/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { Controller } from '@nestjs/common';
-import { User } from 'src/app/models/database/entities/user/user.entity';
+import { User } from 'src/app/database/entities/user/user.entity';
 import { UserService } from 'src/app/services/user/user.service';
 import { AbstractController } from 'src/core/abstract-controller';
 
