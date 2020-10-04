bdfd5055192c54634bc349b726bac64190be09ef
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-10-04 16:51:46 -0300

    chore: change path services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './app/models/database/database.module';
-import { UserService } from './app/models/services/user/user.service';
+import { UserService } from './app/services/user/user.service';
 import { UserController } from './app/controllers/user/user.controller';
 import { userProvider } from './app/models/database/entities/user/user.provider';
 
--- src/app/controllers/user/user.controller.ts
+++ src/app/controllers/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller } from '@nestjs/common';
 import { User } from 'src/app/models/database/entities/user/user.entity';
-import { UserService } from 'src/app/models/services/user/user.service';
+import { UserService } from 'src/app/services/user/user.service';
 import { AbstractController } from 'src/core/abstract-controller';
 
 @Controller('user')
